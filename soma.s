bdd12bd2c38b85543d6c4e218ad9d8f74418e7d4
Allan Felipe <113111468+FelipeWalkerX@users.noreply.github.com>
2023-08-16 12:31:22 -0300

    Soma.s

--- soma.s
+++ soma.s
@@ -8,9 +8,9 @@ li $t0, 5               #load immediate
 li $t1, 10
 add $t2, $t0, $t1       #t2 = 5 + 10
 
-li $v0, 1               #imprimir inteiro (1 = inteiro)
+li $v0, 1               #imprimir inteiro (1 = inteiro) (v0 =  feito para "dar sentido" aos codigos, geralmente vem acompanhado dos codigos de 1 a 10)
 move $a0, $t2           #mover o resultado (t2 = 15) para o a0
-syscall                 #imprimir
+syscall                 #chamada de sistema (dizer ao hardware que encerrei essa parte do codigo)
 
 li $v0, 10              #imprimir a saida de sistema (10 = sair do programa)
-syscall                 #imprimir
+syscall                 #encerrar programa
